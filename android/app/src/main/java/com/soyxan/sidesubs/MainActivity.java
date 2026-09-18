eb273012cc679effdb82a56ce0a3fb67be210b1f
soyxan <vicente.uchan@gmail.com>
2026-09-18 12:56:31 +0200

    Handle Android rotation without reloading WebView

--- android/app/src/main/java/com/soyxan/sidesubs/MainActivity.java
+++ android/app/src/main/java/com/soyxan/sidesubs/MainActivity.java
@@ -3,6 +3,7 @@ package com.soyxan.sidesubs;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
@@ -152,6 +153,12 @@ public class MainActivity extends Activity {
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             );
         }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        enterImmersiveMode();
     }
 
     @Override
