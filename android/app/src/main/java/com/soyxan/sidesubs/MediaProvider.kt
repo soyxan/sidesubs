496e84b2ad99bc43de9c8a347be9382848ac5b85
soyxan <vicente.uchan@gmail.com>
2026-09-22 12:58:30 +0200

    Add generic Android media provider contract

---
+++ android/app/src/main/java/com/soyxan/sidesubs/MediaProvider.kt
@@ -0,0 +1,23 @@
+package com.soyxan.sidesubs
+
+enum class MediaProviderType(val displayName: String) {
+    PLEX("Plex"),
+}
+
+data class ProviderConnection(
+    val provider: MediaProviderType,
+    val serverId: String,
+    val serverName: String,
+    val baseUrl: String,
+    val accessToken: String,
+    val clientIdentifier: String,
+)
+
+interface MediaProvider {
+    val providerType: MediaProviderType
+    val serverName: String
+    fun sessions(): List<PlaybackSession>
+    fun subtitleTracks(mediaId: String): List<SubtitleTrack>
+    fun subtitleTimeline(mediaId: String, track: SubtitleTrack): SubtitleTimeline
+    fun clearSubtitleCache()
+}
